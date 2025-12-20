95cf0880143b428bb3bdf9fc38b503c2abc4f4fc
Chinedu Mgbemena <mgbemena.nedu@gmail.com>
2025-12-20 22:53:17 +0100

    Handled Permissions and initialized a backed for my terraform state files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = ">= 1.0"
+
+  # REAL WORLD CONFIGURATION
+  backend "gcs" {
+    bucket = "gke-devops-terraform-state-strange-mariner-290720" # Use the name you just created
+    prefix = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 provider "google" {
   project = var.project_id
   region  = var.region
